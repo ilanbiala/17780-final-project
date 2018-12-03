a96dd4e475c61893c13ebc7ae9f84a9f12d1da86
Varun Sharma <sharmav39@gmail.com>
2018-12-02 19:39:52 -0500

    Add ProcessingApp and Canvas

--- src/main/java/processingwrapper/Canvas.java
+++ src/main/java/processingwrapper/Canvas.java
@@ -1,5 +1,45 @@
 package processingwrapper;
 
 public class Canvas {
+  private int width, height;
 
+  public Canvas(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public void draw(Drawable drawable, Position pos) {
+
+  }
+
+  public void draw(Shape shape, ShapeSettings shapeSettings, Position pos) {
+
+  }
+
+  public void draw(Image img, Position pos) {
+
+  }
+
+  public void draw(Image img, ImageSettings imgSettings, Position pos) {
+
+  }
+
+  public void draw(Canvas canvas, Position pos) {
+
+  }
+
+  public void fill(java.awt.Color color) {
+
+  }
+
+  void flushAt(int x, int y) {
+
+  }
+
+  public int width() {
+    return this.width;
+  }
+  public int height() {
+    return this.height;
+  }
 }
