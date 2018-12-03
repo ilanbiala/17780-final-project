a557f6b6b8cd18aed5e6dcc65ef59ebb948d549a
Varun Sharma <sharmav39@gmail.com>
2018-12-02 19:54:05 -0500

    implemented draw methods

--- src/main/java/processingwrapper/Canvas.java
+++ src/main/java/processingwrapper/Canvas.java
@@ -7,6 +7,7 @@ public class Canvas {
   private int width, height;
   private List<Drawable> drawables;
   private List<Position> positions;
+  private java.awt.Color backgroundColor;
 
   public Canvas(int width, int height) {
     this.width = width;
@@ -16,27 +17,34 @@ public class Canvas {
   }
 
   public void draw(Drawable drawable, Position pos) {
-
+    this.drawables.add(drawable);
+    this.positions.add(pos);
   }
 
   public void draw(Shape shape, ShapeSettings shapeSettings, Position pos) {
-
+    this.drawables.add(Drawable.ofShape(shape, shapeSettings));
+    this.positions.add(pos);
   }
 
   public void draw(Image img, Position pos) {
-
+    this.drawables.add(Drawable.ofImage(img));
+    this.positions.add(pos);
   }
 
   public void draw(Image img, ImageSettings imgSettings, Position pos) {
-
+    this.drawables.add(Drawable.ofImage(img));
+    this.positions.add(pos);
   }
 
   public void draw(Canvas canvas, Position pos) {
-
+    this.drawables.add(Drawable.ofCanvas(canvas));
+    this.positions.add(pos);
   }
 
   public void fill(java.awt.Color color) {
-
+    this.drawables.clear();
+    this.positions.clear();
+    this.backgroundColor = color;
   }
 
   void commitAt(int x, int y) {
