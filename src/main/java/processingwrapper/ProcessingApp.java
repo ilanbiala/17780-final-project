4094a4c885cab152700a43e99b12cf7846b0ae2b
Varun Sharma <sharmav39@gmail.com>
2018-12-05 12:31:32 -0500

    fix start in ProcessingApp

--- src/main/java/processingwrapper/ProcessingApp.java
+++ src/main/java/processingwrapper/ProcessingApp.java
@@ -4,7 +4,6 @@ import processing.core.PApplet;
 
 import java.util.function.BiFunction;
 
-
 /**
  * {@link ProcessingApp} is the interface implemented by client applications.
  * {@link #start } should be called to begin running a ProcessingApp
@@ -14,28 +13,34 @@ public interface ProcessingApp {
    * Starts running the supplied ProcessingApp
    *
    * @param appConstructor A constructor which creates a ProcessingApp given the width and height
-   * @param width          The window width
-   * @param height         The window height
+   * @param windowWidth    The window width
+   * @param windowHeight   The window height
    */
-  static void start(BiFunction<Integer, Integer, ProcessingApp> appConstructor, int width, int height) {
-
-    Canvas mainCanvas = new Canvas(width, height);
+  static void start(BiFunction<Integer, Integer, ProcessingApp> appConstructor, int windowWidth, int windowHeight) {
+    Canvas mainCanvas = new Canvas(windowWidth, windowHeight);
 
     class WrapperPApplet extends PApplet {
       private ProcessingApp app;
 
-      public void setup() {
-        size(width, height);
-        app = appConstructor.apply(width, height);
+      @Override
+      public void settings() {
+        size(windowWidth, windowHeight);
       }
 
+      @Override
+      public void setup() {
+        app = appConstructor.apply(windowWidth, windowHeight);
+      }
+
+      @Override
       public void draw() {
         app.drawFrame(mainCanvas);
         mainCanvas.commitAt(this, 0, 0);
       }
     }
 
-    PApplet.main(WrapperPApplet.class);
+    String[] args = {"--location=0,0", "WrapperPApplet"};
+    PApplet.runSketch(args, new WrapperPApplet());
   }
 
   /**
